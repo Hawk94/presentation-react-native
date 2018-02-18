e63f2822ebc24bcf0d418f07d3118dc24429d8cc
Tom Miller <tmiller@thecollective.co.uk>
2018-02-18 18:00:48 +0000

    Presentation theme and ideas

--- presentation/index.js
+++ presentation/index.js
@@ -14,45 +14,60 @@ import {
   Text
 } from "spectacle";
 
+import Logo from "./../assets/logo.svg";
+
 // Import theme
 import createTheme from "spectacle/lib/themes/default";
 
 // Require CSS
 require("normalize.css");
 
-const theme = createTheme({
-  primary: "white",
-  secondary: "#1F2022",
-  tertiary: "#03A9FC",
-  quarternary: "#CECECE"
-}, {
-  primary: "Montserrat",
-  secondary: "Helvetica"
-});
+const theme = createTheme(
+  {
+    primary: "#4a5665",
+    secondary: "#ffffff",
+    blueyGrey: "#9dabba",
+    battleshipGrey: "#6e7b8c",
+    greenAction: "#29dc8d",
+    greenApple: "#42c627",
+    orange: "#ff7557",
+    blue: "#60b6eb",
+    pink: "#e173a9",
+    green: "#51cfc5"
+  },
+  {
+    primary: "Varela",
+    secondary: "Helvetica"
+  }
+);
 
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
-        <Slide transition={["zoom"]} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+      <Deck
+        transition={["zoom", "slide"]}
+        transitionDuration={500}
+        theme={theme}
+      >
+        <Slide transition={["zoom"]} bgColor="orange">
+          <Heading size={1} fit caps lineHeight={1} textColor="primary">
+            The Collective ft. React Native
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+          <Text margin="10px 0 0" textColor="secondary" size={1} bold>
+            Alive Together
           </Text>
         </Slide>
-        <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
+        <Slide transition={["fade"]} bgColor="black">
+          <Heading size={6} textColor="primary" caps>
+            Who are we?
+          </Heading>
+          <br />
+          <img src={Logo} height="100px" width="100px" />
         </Slide>
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
+          <Heading size={6} textColor="secondary" caps>
+            Standard List
+          </Heading>
           <List>
             <ListItem>Item 1</ListItem>
             <ListItem>Item 2</ListItem>
